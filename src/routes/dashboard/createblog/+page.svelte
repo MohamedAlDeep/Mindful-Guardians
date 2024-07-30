43a8e31fe2d0a80f17f3ac426ffe57a143c2d9b6
Mohamed Samir <mohamedsamir22446688@gmail.com>
2024-07-31 01:22:21 +0300

    blog / library
    Added blogs and library

---
+++ src/routes/dashboard/createblog/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import NavBar from '../../../components/navBar/navBar.svelte';
+</script>
+
+<NavBar cookie={true}/>
+
+<form method="POST" action="?/blog" class="auth-form">
+    <h2>Title</h2>
+    <input type="text" name="title">
+    <h2>Content</h2>
+    <textarea class="content" name="content" id=""></textarea>
+    <button type="submit">Submit</button>
+</form>
+
+
+<style>
+    .content{
+        height: 200px;
+        resize: vertical;
+    }
+</style>
